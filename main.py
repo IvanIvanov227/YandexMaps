5160a51e0df1489986ae6d997dd90f639a79b463
Negolng <114674976+Negolng@users.noreply.github.com>
2024-02-03 13:40:18 +0500

    чёт сделал
    сами угадываете
    
    без мата

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 import requests
 
 
+# kurwa
+
 def error(res):
     print("Ошибка выполнения запроса:")
     print("Http статус:", res.status_code, "(", res.reason, ")")
@@ -11,12 +13,14 @@ def error(res):
 
 def load_data():
     try:
-        coords = map(float, input('Введите координаты: ').split())
-        scale = map(float, input('Введите мастштаб: ').split())
-    except Exception:
+        # coords = list(map(float, input('Введите координаты: ').split()))
+        # scale = list(map(float, input('Введите мастштаб: ').split()))
+        cords = 60, 57
+        scale = 1, 1
+    except ValueError:
         print('Вводите нормальные данные')
     else:
-        return coords, scale
+        return cords, scale
 
 
 def load_image(coords, scale):
@@ -27,24 +31,70 @@ def load_image(coords, scale):
         'l': 'map'
     }
     response = requests.get(static_api_server, params=params_static)
+    print('REQUEST')
 
     if not response:
         error(response)
-
     with open('map.png', 'wb') as file:
         file.write(response.content)
+
+
+def check_scale(scale):
+    scale_1, scale_2 = scale
+    new_scale = scale
+    if scale_1 <= 0:
+        new_scale[0] = 0.05
+    if scale_2 <= 0:
+        new_scale[1] = 0.05
+    if scale_1 >= 90:
+        new_scale[0] = 89
+    if scale_2 >= 90:
+        new_scale[1] = 89
+    return new_scale
 
 
 def main():
     coords, scale = load_data()
     load_image(coords, scale)
+    
 
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
-    screen.blit(pygame.image.load('map.png'), (0, 0))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    running = True
+    map_im = pygame.image.load('map.png')
+
+    scale_step = 0.4
+    jump_scare = 0.05
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_PAGEUP]:
+                    scale = check_scale([scale[0] + scale_step, scale[1] + scale_step])
+
+                elif keys[pygame.K_PAGEDOWN]:
+                    scale = check_scale([scale[0] - scale_step, scale[1] - scale_step])
+
+                elif keys[pygame.K_UP]:
+                    coords = [coords[0], coords[1] + jump_scare]
+                elif keys[pygame.K_DOWN]:
+                    coords = [coords[0], coords[1] - jump_scare]
+                elif keys[pygame.K_LEFT]:
+                    coords = [coords[0] - jump_scare, coords[1]]
+                elif keys[pygame.K_RIGHT]:
+                    coords = [coords[0] + jump_scare, coords[1]]
+
+                load_image(coords, scale)
+                map_im = pygame.image.load('map.png')
+
+
+
+
+        screen.blit(map_im, (0, 0))
+        pygame.display.flip()
     pygame.quit()
 
 
