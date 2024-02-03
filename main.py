26a7a8937fd3c4bd33abe60fc297979ccd262ddc
Negolng <mahetov.pavel@gmail.com>
2024-02-03 18:16:29 +0500

    Добавил движение стрелочками

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('GUI.ui', self)
         self.map = QPixmap()
-        self.zoom = 12
+        self.zoom = 17
+        self.cords = [60.583335, 56.964456]
+
+        self.move_speed = 0.001
         self.connect_buttons()
         self.update_map()
 
@@ -28,13 +31,25 @@ class MainWindow(QMainWindow):
         # тут кнопочки соеденяем
         # Пайчарм на отсуствие коннекта ругается, но он лох слепой просто
         zoom_in = QShortcut(QKeySequence(Qt.Key.Key_PageUp), self)
-        zoom_in.activated.connect(self.zoomin_map)
+        zoom_in.activated.connect(self.zoomout_map)
 
         zoom_out = QShortcut(QKeySequence(Qt.Key.Key_PageDown), self)
-        zoom_out.activated.connect(self.zoomout_map)
+        zoom_out.activated.connect(self.zoomin_map)
+
+        move_left = QShortcut(QKeySequence(Qt.Key.Key_Left), self)
+        move_left.activated.connect(self.move_left)
+
+        move_right = QShortcut(QKeySequence(Qt.Key.Key_Right), self)
+        move_right.activated.connect(self.move_right)
+
+        move_up = QShortcut(QKeySequence(Qt.Key.Key_Up), self)
+        move_up.activated.connect(self.move_up)
+
+        move_down = QShortcut(QKeySequence(Qt.Key.Key_Down), self)
+        move_down.activated.connect(self.move_down)
 
     def update_map(self):
-        self.map.loadFromData(self.load_map([60, 57], self.zoom, 'map'))
+        self.map.loadFromData(self.load_map(self.cords, self.zoom, 'map'))
         # не смотрим, что PyCharm ругается, ибо пайчарм - тот ещё дурачок, мы эту кнопку в uic.loadui в __init__ делали
         self.map_label.setPixmap(self.map)
 
@@ -46,6 +61,22 @@ class MainWindow(QMainWindow):
     def zoomout_map(self):
         self.zoom -= 1
         self.check_zoom()
+        self.update_map()
+
+    def move_left(self):
+        self.cords = [self.cords[0] - self.move_speed, self.cords[1]]
+        self.update_map()
+
+    def move_right(self):
+        self.cords = [self.cords[0] + self.move_speed, self.cords[1]]
+        self.update_map()
+
+    def move_up(self):
+        self.cords = [self.cords[0], self.cords[1] + self.move_speed]
+        self.update_map()
+
+    def move_down(self):
+        self.cords = [self.cords[0], self.cords[1] - self.move_speed]
         self.update_map()
 
     @staticmethod
